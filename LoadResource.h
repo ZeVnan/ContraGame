9a11f9a410508c16164c4ce8fe5ca6cc73ba19a1
KIWI\ADMIN <21522217@gm.uit.edu.vn>
2023-04-01 15:23:59 +0700

    <update>: add ani aircraft, cannon, rifleman

--- LoadResource.h
+++ LoadResource.h
@@ -13,14 +13,23 @@
 
 #include "Bill.h"
 #include "Bullet.h"
+#include "Aircraft.h"
+#include "Cannon.h"
 
 #include "SampleKeyEventHandler.h"
 
 #define ID_TEX_BILL 0
 #define ID_TEX_BULLET 1
+#define ID_TEX_AIRCRAFT 3
+#define ID_TEX_CANNON 4
+#define ID_TEX_RIFLEMAN 5
 
 #define TEXTURE_PATH_BILL L"Images\\bill_animation.png"
 #define TEXTURE_PATH_BULLET L"Images\\Bullets.png"
+#define TEXTURE_PATH_AIRCRAFT L"Images\\aircraft.png"
+#define TEXTURE_PATH_CANNON L"Images\\Cannon_all"
 
 void CreateBillAni(CTextures*& textures, CSprites*& sprites, CAnimations*& animations);
 void CreateBulletAni(CTextures*& textures, CSprites*& sprites, CAnimations*& animations);
+void CreateAircraftAni(CTextures*& textures, CSprites*& sprites, CAnimations*& animations);
+void CreateCannonAni(CTextures*& textures, CSprites*& sprites, CAnimations*& animations);
---
+++ Rifleman.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "GameObject.h"
+
+#define RIFLEMAN_ANI_NORMAL 400
+#define RIFLEMAN_ANI_SHOOT 410
+#define RIFLEMAN_ANI_AIM_UP 420
+#define RIFLEMAN_ANI_SHOOT_UP 430
+#define RIFLEMAN_ANI_AIM_DOWN 440
+#define RIFLEMAN_ANI_HIDE 450
+#define RIFLEMAN_ANI_EXPOSE 460
+
+
+class Rifleman
+{
+
+};
+
